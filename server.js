485f4f904341d46647271cf3a6754bfce730872c
Alexander <maniacallion@gmail.com>
2019-05-06 22:46:43 -0500

    setup of depnedency variable, app.listen to check if server lives, and const client

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+require('dotenv').config();
+const superagent = require('superagent');
+const { Client } = require('pg');
+const express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 3000,
+  HEARTHSTONE_API_KEY = process.env.HEARTHSTONE_API_KEY;
+
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', error => console.log(error));
+
+/* console log if server lives */ 
+app.listen(PORT, () => console.log(`IT LIVES!!! on ${PORT}`));
