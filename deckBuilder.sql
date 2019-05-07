b7fa617b8cfcbce74bc9a8a75fe1d8029f3ec37a
Ian Smith <ianmsmith17@gmail.com>
2019-05-07 09:47:11 -0500

    fixed typos in schema

--- deckBuilder.sql
+++ deckBuilder.sql
@@ -1,31 +1,31 @@
 
 CREATE TABLE users (
   userId SERIAL PRIMARY KEY, 
-  userName VARCHAR (100),
-  password CHAR (60)
+  userName VARCHAR(100),
+  password CHAR(60)
 );
 
 CREATE TABLE decks (
   decksId SERIAL PRIMARY KEY,
-  deckName VARCHAR (15),
+  deckName VARCHAR(15),
   class VARCHAR(100),
   userId INTEGER NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users (userId)
-);
-
-CREATE TABLE decksCards (
-  decskId INTEGER NOT NULL,
-  FOREIGN KEY (decskId) REFERENCES decks (decksId),
-  cardsId INTEGER NOT NULL, 
-  FOREIGN KEY (cardsId)  REFERENCES cards (cardsId)
+  FOREIGN KEY(userId) REFERENCES users(userId)
 );
 
 CREATE TABLE cards (
   cardsId SERIAL PRIMARY KEY,
-  name VARCHAR (255),
-  type VARCHAR (255),
-  class VARCHAR (255),
+  name VARCHAR(255),
+  type VARCHAR(255),
+  class VARCHAR(255),
   cost INTEGER NOT NULL,
-  img VARCHAR (255)
-
+  img VARCHAR(255)
 );
+
+CREATE TABLE decksCards (
+  decksId INTEGER NOT NULL,
+  FOREIGN KEY(decksId) REFERENCES decks(decksId),
+  cardsId INTEGER NOT NULL, 
+  FOREIGN KEY(cardsId) REFERENCES cards(cardsId)
+);
+
