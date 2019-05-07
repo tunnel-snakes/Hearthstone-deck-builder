8bdb182dec2d51bb74e19c93e06a3c63550dad10
Alexander <maniacallion@gmail.com>
2019-05-06 22:47:53 -0500

    function handleError added to be used for handling errors later on

--- server.js
+++ server.js
@@ -13,5 +13,11 @@ const client = new Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', error => console.log(error));
 
+//Error
+function handleError (error, response) {
+  console.log('error', error);
+  if(response) response.status(500).send('Something went wrong');
+}
+
 /* console log if server lives */ 
 app.listen(PORT, () => console.log(`IT LIVES!!! on ${PORT}`));
