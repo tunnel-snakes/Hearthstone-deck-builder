5556465c73b410675df1de267b3d3aa6b9d6a185
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-05-09 18:59:21 -0500

    added more styling to decks page

---
+++ public/app.js
@@ -0,0 +1,15 @@
+function hamburgerSwitch() {
+    let toggle = document.getElementById("myLinks");
+    if (toggle.style.display === "block") {
+      toggle.style.display = "none";
+    } else {
+      toggle.style.display = "block";
+    }
+}
+
+document.getElementById("togglingForm").addEventListener('click', formToggle);
+
+function formToggle() {
+  let toggle = document.getElementById("classToggle");
+  toggle.classList.toggle("hidden-toggle");
+}
